cddedba1502e2041379ed6612a9cf51cf3dee6b3
Harshita222 <kumariharsita501@gmail.com>
2024-07-06 20:14:47 +0530

    do

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import UserForm from '../components/UserForm';
+import UserInfo from '../components/UserInfo';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Redux Application Demo</h1>
+      <UserForm />
+      <UserInfo />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
